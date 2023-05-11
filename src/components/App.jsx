224655ba7a95a0f0988d0120cc1a25d103632a3a
K-Basket <koshykkv@gmail.com>
2023-05-11 18:35:52 +0300

    add task-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,9 +12,23 @@ export class App extends Component {
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-5', name: 'K-Basket', number: '339-49-59' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const getContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (getContacts) {
+      this.setState({ contacts: getContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   formSubmitHandler = data => {
     // console.log('data', data);
@@ -63,9 +77,6 @@ export class App extends Component {
   };
 
   render() {
-    // console.log(this.state.contacts);
-    // console.log(this.state.filter);
-
     return (
       <>
         <h1>Phonebook</h1>
